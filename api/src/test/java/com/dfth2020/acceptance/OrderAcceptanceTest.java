b02b9ee8deb77f7dc7f0e2c8c9e6471a3e91179c
ashley <ashleykubera@gmail.com>
2020-09-26 10:38:48 -0400

    Added test to ensure order item is not null

--- api/src/test/java/com/dfth2020/acceptance/OrderAcceptanceTest.java
+++ api/src/test/java/com/dfth2020/acceptance/OrderAcceptanceTest.java
@@ -1,12 +1,16 @@
 package com.dfth2020.acceptance;
 
 import com.dfth2020.client.api.OrderItemApi;
+import com.dfth2020.client.model.OrderItem;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class OrderAcceptanceTest extends BaseAcceptanceTest {
@@ -17,6 +21,13 @@ public class OrderAcceptanceTest extends BaseAcceptanceTest {
     public void before() {
         orderItemApi = new OrderItemApi();
         orderItemApi.setApiClient(getApiClient());
+    }
+
+    @Test
+    public void ensureGetOrderItemReturns200ForFoundOrderItem() {
+        OrderItem orderItem = orderItemApi.orderOrderIdItemOrderItemIdGet(UUID.fromString("ddfc229c-3c4f-4c5f-a59f-d7a1d8da8675"), UUID.fromString("41b73992-3eac-4583-b11e-1a8b1fcad370"));
+
+        assertThat(orderItem).isNotNull();
     }
 
     @Test
