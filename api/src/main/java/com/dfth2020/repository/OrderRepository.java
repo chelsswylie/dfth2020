ae40dc8fedfcbabd8576c7fbb4021132fa63f8ae
ashley <ashleykubera@gmail.com>
2020-09-26 11:54:59 -0400

    Added get order API

--- api/src/main/java/com/dfth2020/repository/OrderRepository.java
+++ api/src/main/java/com/dfth2020/repository/OrderRepository.java
@@ -2,8 +2,13 @@ package com.dfth2020.repository;
 
 import com.dfth2020.entity.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface OrderRepository extends JpaRepository<OrderEntity, UUID> {
+
+    @Query("SELECT oI FROM customer_order oI WHERE oI.orderId=:orderId")
+    Optional<OrderEntity> findById(UUID orderId);
 }
--- api/src/test/java/com/dfth2020/acceptance/OrderAcceptanceTest.java
+++ api/src/test/java/com/dfth2020/acceptance/OrderAcceptanceTest.java
@@ -2,6 +2,7 @@ package com.dfth2020.acceptance;
 
 import com.dfth2020.client.api.OrderApi;
 import com.dfth2020.client.api.OrderItemApi;
+import com.dfth2020.client.model.Order;
 import com.dfth2020.client.model.OrderItem;
 import com.dfth2020.client.model.Orders;
 import com.dfth2020.client.model.ProductionStep;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.client.HttpClientErrorException;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
@@ -44,10 +46,20 @@ public class OrderAcceptanceTest extends BaseAcceptanceTest {
     }
 
     @Test
+    public void ensureGetOrderReturns200ForFoundOrder() {
+        Order order = orderApi.getOrder(UUID.fromString("ddfc229c-3c4f-4c5f-a59f-d7a1d8da8675"));
+
+        assertThat(order).isNotNull();
+        assertThat(order.getId()).isEqualTo(UUID.fromString("ddfc229c-3c4f-4c5f-a59f-d7a1d8da8675"));
+    }
+
+    @Test
     public void ensureGetOrderItemReturns200ForFoundOrderItem() {
         OrderItem orderItem = orderItemApi.getOrderItem(UUID.fromString("ddfc229c-3c4f-4c5f-a59f-d7a1d8da8675"), UUID.fromString("41b73992-3eac-4583-b11e-1a8b1fcad370"));
 
         assertThat(orderItem).isNotNull();
+        assertThat(orderItem.getId()).isEqualTo(UUID.fromString("41b73992-3eac-4583-b11e-1a8b1fcad370"));
+        assertThat(orderItem.getProductionSteps().get(0).getOrder()).isEqualTo(new BigDecimal(1));
     }
 
     @Test
